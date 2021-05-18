a4c8e3b580210b25108f948c7be94e4520785ecb
Darryl Yeo <dev@darryl-yeo.com>
2021-05-18 12:26:47 -0700

    Add option to hide slider

--- app/src/components/TokenAmountSelect.svelte
+++ app/src/components/TokenAmountSelect.svelte
@@ -6,6 +6,7 @@
 	export let amount
 
 
+	export let showSlider = true
 	// let isFocused = false
 	// let textInputValue = String(amount)
 
@@ -52,20 +53,24 @@
 	/> -->
 	<span>{token.symbol}</span>
 </div>
-<Row>
-	<Button small fill color="gray" onClick={() => amount = 0.10 * maxAmount}>10%</Button>
-	<Button small fill color="gray" onClick={() => amount = 0.25 * maxAmount}>25%</Button>
-	<Button small fill color="gray" onClick={() => amount = 0.50 * maxAmount}>50%</Button>
-	<Button small fill color="gray" onClick={() => amount = 0.75 * maxAmount}>75%</Button>
-	<Button small fill color="gray" onClick={() => amount = 1.00 * maxAmount}>max</Button>
-</Row>
-<Range
-	value={amount} onRangeChange={_ => amount = _}
-	min={0}
-	max={maxAmount}
-	color={token.color}
-	style="--f7-range-knob-background-image: url('{token.image}')"
-	scaleSteps={5}
-	scaleSubSteps={20}
-	formatScaleLabel={value => value === 100 ? 'max' : value}
-/>
+
+{#if showSlider}
+	<Row>
+		<Button small fill color="gray" onClick={() => amount = 0.10 * maxAmount}>10%</Button>
+		<Button small fill color="gray" onClick={() => amount = 0.25 * maxAmount}>25%</Button>
+		<Button small fill color="gray" onClick={() => amount = 0.50 * maxAmount}>50%</Button>
+		<Button small fill color="gray" onClick={() => amount = 0.75 * maxAmount}>75%</Button>
+		<Button small fill color="gray" onClick={() => amount = 1.00 * maxAmount}>max</Button>
+	</Row>
+
+	<Range
+		value={amount} onRangeChange={_ => amount = _}
+		min={0}
+		max={maxAmount}
+		color={token.color}
+		style="--f7-range-knob-background-image: url('{token.image}')"
+		scaleSteps={5}
+		scaleSubSteps={20}
+		formatScaleLabel={value => value === 100 ? 'max' : value}
+	/>
+{/if}
