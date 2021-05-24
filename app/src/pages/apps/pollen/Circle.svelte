bbd33ec589a6f5ecdfeeefb43d6b557813a900d5
Darryl Yeo <dev@darryl-yeo.com>
2021-05-24 14:36:58 -0700

    Fix build error "Assigning to rvalue"

--- app/src/pages/apps/pollen/Circle.svelte
+++ app/src/pages/apps/pollen/Circle.svelte
@@ -5,7 +5,9 @@
 
 	let pollenID
 	import { onMount } from 'svelte'
-	onMount(() => ({pollenID} = f7route.params))
+	onMount(() => {
+		pollenID = f7route.params.pollenID
+	})
 
 
 	import { Navbar, Page } from 'framework7-svelte'
@@ -14,4 +16,6 @@
 <Page>
 	<Navbar	title="Pollen Circle" backLink="Pollen">
 	</Navbar>
+
+	<h1>#{pollenID}</h1>
 </Page>
