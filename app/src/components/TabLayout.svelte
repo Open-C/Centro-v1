434e51c6d902581c4de3dc11cd7ca5dff80ebe50
Darryl Yeo <dev@darryl-yeo.com>
2021-05-26 15:17:58 -0700

    Create <TabLayout> component

---
+++ app/src/components/TabLayout.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+	let id = 0
+</script>
+
+
+<script>
+	let tabInstanceID = id++
+
+
+	export let tabs = {} // Record<id, title>
+
+	export let animated = false
+	export let swipeable = false
+	export let routable = false
+	export let swiperParams = false
+
+
+    import { Button, Segmented, Tab, Tabs } from 'framework7-svelte'
+</script>
+
+
+<Segmented strong>
+	{#each Object.entries(tabs) as [tabID, title] (tabID)}
+		<Button tabLink="#{tabID}-{tabInstanceID}">{title}</Button>
+	{/each}
+</Segmented>
+
+<Tabs {animated} {swipeable} {routable} {swiperParams}>
+	{#each Object.entries(tabs) as [tabID, title] (tabID)}
+		<Tab id="{tabID}-{tabInstanceID}">
+			<slot tab={tabID} />
+		</Tab>
+	{/each}
+</Tabs>
--- app/src/components/TokenDepositOrWithdraw.svelte
+++ app/src/components/TokenDepositOrWithdraw.svelte
@@ -5,58 +5,25 @@
 	export let maxWithdrawAmount
 
 
-	let mode = 'None'
-
-
 	import { tokenBalances } from '../data/tokenBalances'
 
 
-	import { Row, Button, Col, Segmented, Block } from 'framework7-svelte'
+	import { Block } from 'framework7-svelte'
+	import TabLayout from './TabLayout.svelte'
 	import TokenDeposit from './TokenDeposit.svelte'
 	import TokenWithdraw from './TokenWithdraw.svelte'
 </script>
 
 
-<Segmented strong>
-	<Button active={mode === 'Deposit'} onClick={() => mode = 'Deposit'}>Deposit</Button>
-	<Button active={mode === 'Withdraw'} onClick={() => mode = 'Withdraw'}>Withdraw</Button>
-</Segmented>
-
-{#if mode === 'None'}
-{:else if mode === 'Deposit'}
-	<Block inset strong>
-		<TokenDeposit
-			token={depositToken}
-			maxAmount={maxDepositAmount}
-		/>
-	</Block>
-{:else if mode === 'Withdraw'}
-	<Block inset strong>
-		<TokenWithdraw
-			token={withdrawToken}
-			maxAmount={maxWithdrawAmount}
-		/>
-	</Block>
-{/if}
-
-
-<!-- <Block inset strong>
-	<Segmented strong>
-		<Button active={mode === 'Deposit'} onClick={() => mode = 'Deposit'}>Deposit</Button>
-		<Button active={mode === 'Withdraw'} onClick={() => mode = 'Withdraw'}>Withdraw</Button>
-	</Segmented>
-</Block>
-
-<div class="stack">
-	{#if mode === 'None'}
-	{:else if mode === 'Deposit'}
+<TabLayout tabs={{'request': 'Request', 'send': 'Send'}} let:tab>
+	{#if tab === 'request'}
 		<Block inset strong>
 			<TokenDeposit
 				token={depositToken}
 				maxAmount={maxDepositAmount}
 			/>
 		</Block>
-	{:else if mode === 'Withdraw'}
+	{:else if tab === 'send'}
 		<Block inset strong>
 			<TokenWithdraw
 				token={withdrawToken}
@@ -64,32 +31,4 @@
 			/>
 		</Block>
 	{/if}
-</div> -->
-
-
-<!-- <div class="stack">
-	{#if mode === 'None'}
-		<Row>
-			<Col>
-				<Button fill onClick={() => mode = 'Deposit'}>Deposit</Button>
-			</Col>
-			<Col>
-				<Button fill onClick={() => mode = 'Withdraw'}>Withdraw</Button>
-			</Col>
-		</Row>
-	{:else if mode === 'Deposit'}
-		<Block inset strong>
-			<TokenDeposit
-				token={depositToken}
-				maxAmount={maxDepositAmount}
-			/>
-		</Block>
-	{:else if mode === 'Withdraw'}
-		<Block inset strong>
-			<TokenWithdraw
-				token={withdrawToken}
-				maxAmount={maxWithdrawAmount}
-			/>
-		</Block>
-	{/if}
-</div> -->
+</TabLayout>
