4a294fb42de75832320cf8d20e596f54e1eb1231
Darryl Yeo <dev@darryl-yeo.com>
2021-06-03 02:09:04 -0700

    Add chart, goal, interest earned, deposit/withdraw actions, and transaction history

---
+++ app/src/data/actions.js
@@ -0,0 +1,26 @@
+export const actionVerbs = {
+    'Receive': {
+        pastTense: 'Received',
+        presentTense: 'Receiving'
+    },
+    'Send': {
+        pastTense: 'Sent',
+        presentTense: 'Sending'
+    },
+    'Buy': {
+        pastTense: 'Bought',
+        presentTense: 'Buying'
+    },
+    'Sell': {
+        pastTense: 'Sold',
+        presentTense: 'Selling'
+    },
+    'Deposit': {
+        pastTense: 'Deposited',
+        presentTense: 'Depositing'
+    },
+    'Withdraw': {
+        pastTense: 'Withdrew',
+        presentTense: 'Withdrawing'
+    }
+}
--- app/src/pages/Assets.svelte
+++ app/src/pages/Assets.svelte
@@ -52,9 +52,60 @@
 	import { tokenBalances } from '../data/tokenBalances'
 
 
-	import { Page, Navbar, NavLeft, NavTitle, NavTitleLarge, NavRight, Link, Toolbar, Block, BlockTitle, List, ListItem, Row, Col, Button, AccordionContent, Card } from 'framework7-svelte'
+	let transactions = [
+		{
+			action: 'Send',
+			token: baseAssets[0],
+			amount: 10,
+			price: 10,
+			timestamp: 1612345678901
+		},
+		{
+			action: 'Withdraw',
+			token: baseAssets[1],
+			amount: 11,
+			price: 11.23,
+			timestamp: 1622345678901
+		},
+		{
+			action: 'Deposit',
+			token: baseAssets[2],
+			amount: 600,
+			price: 4.5,
+			timestamp: 1621234567890
+		},
+		{
+			action: 'Sell',
+			token: baseAssets[0],
+			amount: 10,
+			price: 10,
+			timestamp: 1622345678901
+		},
+		{
+			action: 'Buy',
+			token: baseAssets[1],
+			amount: 100,
+			price: 20.09,
+			timestamp: 1622345678901
+		},
+		{
+			action: 'Receive',
+			token: baseAssets[2],
+			amount: 10,
+			price: 235,
+			timestamp: 1622345678901
+		}
+	]
+
+
+	import { actionVerbs } from '../data/actions'
+
+
+	import { Page, Navbar, NavLeft, NavTitle, NavTitleLarge, NavRight, Link, Toolbar, Block, BlockTitle, List, ListItem, Row, Col, Button, AccordionContent, Card, Progressbar, Icon } from 'framework7-svelte'
 	import CurrentWallet from '../components/CurrentWallet.svelte'
+	import TokenSend from '../components/TokenSend.svelte'
 	import TokenRequestOrSend from '../components/TokenRequestOrSend.svelte'
+	import PriceChart from '../components/PriceChart.svelte'
 </script>
 
 <Page name="assets">
@@ -67,6 +118,28 @@
 			<CurrentWallet />
 		</svelte:fragment>
 	</Navbar>
+
+	<Block strong inset>
+		<h2>$972.65</h2>
+		<PriceChart />
+	</Block>
+
+	<Block>
+		<Row>
+			<Col>
+				<Block strong>
+					<p><strong>Goal</strong>: Donate $10/mo</p>
+					<Progressbar progress={70} />
+				</Block>
+			</Col>
+			<Col>
+				<Block strong>
+					<h4>Total Interest Earned</h4>
+					<p>$12.34</p>
+				</Block>
+			</Col>
+		</Row>
+	</Block>
 
 	{#each sections as {title, data: tokens}}
 		<BlockTitle medium>{title}</BlockTitle>
@@ -86,4 +159,53 @@
 			{/each}
 		</List>
 	{/each}
+
+	<BlockTitle medium>Deposit/Withdraw</BlockTitle>
+	<List inset>
+		<ListItem
+			link="/apps/bank-account"
+			title="Bank Account"
+			footer="Deposit/withdraw from a U.S. bank account"
+		>
+			<Icon slot="media" f7="building_2_fill" />
+		</ListItem>
+
+		<ListItem
+			link="/apps/bank-account"
+			title="Mobile Money"
+			footer="Deposit/withdraw from MPesa, MTN, ZamPay"
+		>
+			<Icon slot="media" f7="device_tablet_portrait" />
+		</ListItem>
+
+		<ListItem accordionItem
+			title="External Wallet"
+			footer="Send tokens to another Centro/Celo wallet"
+		>
+			<Icon slot="media" f7="creditcard_fill" />
+			<AccordionContent>
+				<Block>
+					<TokenSend availableTokens={baseAssets} />
+				</Block>
+			</AccordionContent>
+		</ListItem>
+	</List>
+
+	<BlockTitle medium>Past Transactions</BlockTitle>
+	<List accordionList inset>
+		{#each transactions as transaction}
+			<ListItem accordionItem
+				title="{actionVerbs[transaction.action].pastTense} {transaction.amount} {transaction.token.symbol}"
+				footer={new Date(transaction.timestamp).toLocaleDateString()}
+				after="${transaction.price}"
+			>
+				<img slot="media" src={transaction.token.image} height="40" />
+				<AccordionContent>
+					<Block>
+						<Button fill>View in Block Explorer</Button>
+					</Block>
+				</AccordionContent>
+			</ListItem>
+		{/each}
+	</List>
 </Page>
